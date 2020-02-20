81b37656682597c70323e1f81b33b348fe866aa8
Khalil Hamani <hamani_khalil@yahoo.fr>
2020-02-20 17:46:34 +0100

    First commit

---
+++ src/main/java/com/nco/troli/TroliApplication.java
@@ -0,0 +1,13 @@
+package com.nco.troli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TroliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TroliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nco/troli/TroliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nco.troli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TroliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
